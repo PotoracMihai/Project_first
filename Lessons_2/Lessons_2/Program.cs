4d78eb5dea97c639929535d2033bc2e6bb5c84c3
PotoracMihai <potoracmihai74@gmail.com>
2021-08-20 17:51:29 +0300

    lesson2

---
+++ Lessons_2/Lessons_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lessons_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
